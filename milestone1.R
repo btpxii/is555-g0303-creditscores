d05d7d916d53e6b8171af408b67d8ce94513d756
JDMestanzy <91694489+JDMestanzy@users.noreply.github.com>
2024-02-28 23:38:04 -0700

    Update milestone1.R
    FULL SCRIPT

--- milestone1.R
+++ milestone1.R
@@ -208,8 +208,148 @@ column_types <- sapply(data_raw, function(x) {
 summary_df <- data.frame(Column = names(data_raw), Type = column_types)
 print(summary_df)
 
+# BY BRADEN:
+#Stats Analysis
+#Num_Credit_Inquires
+Num_Credit_Inquiries_Anova <- aov(Num_Credit_Inquiries ~ Credit_Score, data = data_raw)
+Num_Credit_Inquiries_Anova_Table <- summary(Num_Credit_Inquiries_Anova)
+Num_Credit_Inquiries_Anova_Table_var1 <- Num_Credit_Inquiries_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Num_Credit_Inquiries_Anova_var2 <- sum(Num_Credit_Inquiries_Anova_Table[[1]][, "Sum Sq"])
+Num_Credit_Inquiries_Anova_r_squared <- Num_Credit_Inquiries_Anova_Table_var1 / Num_Credit_Inquiries_Anova_var2
 
+#Annual_Income - Unavailable due to NA's and skewness in Annual_Income
+Annual_Income_Anova <- aov(Annual_Income ~ Credit_Score, data = na.omit(data_raw[, c("Annual_Income", "Credit_Score")]))
 
+#Num_Bank_Accounts
+Num_Bank_Accounts_Anova <- aov(Num_Bank_Accounts ~ Credit_Score, data = data_raw)
+Num_Bank_Accounts_Anova_Table <- summary(Num_Bank_Accounts_Anova)
+Num_Bank_Accounts_Anova_Table_var1 <- Num_Bank_Accounts_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Num_Bank_Accounts_Anova_var2 <- sum(Num_Bank_Accounts_Anova_Table[[1]][, "Sum Sq"])
+Num_Bank_Accounts_Anova_r_squared <- Num_Bank_Accounts_Anova_Table_var1 / Num_Bank_Accounts_Anova_var2
+
+#Num_Credit_Card
+Num_Credit_Card_Anova <- aov(Num_Credit_Card ~ Credit_Score, data = data_raw)
+Num_Credit_Card_Anova_Table <- summary(Num_Credit_Card_Anova)
+Num_Credit_Card_Anova_Table_var1 <- Num_Credit_Card_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Num_Credit_Card_Anova_var2 <- sum(Num_Credit_Card_Anova_Table[[1]][, "Sum Sq"])
+Num_Credit_Card_Anova_r_squared <- Num_Credit_Card_Anova_Table_var1 / Num_Credit_Card_Anova_var2
+
+#Interest_Rate
+Interest_Rate_Anova <- aov(Interest_Rate ~ Credit_Score, data = data_raw)
+Interest_Rate_Anova_Table <- summary(Interest_Rate_Anova)
+Interest_Rate_Anova_Table_var1 <- Interest_Rate_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Interest_Rate_Anova_var2 <- sum(Interest_Rate_Anova_Table[[1]][, "Sum Sq"])
+Interest_Rate_Anova_r_squared <- Interest_Rate_Anova_Table_var1 / Interest_Rate_Anova_var2
+
+#Num_of_Loan - Needs additioanl data cleaning
+Num_of_Loan_Anova <- aov(Num_of_Loan ~ Credit_Score, data = data_raw)
+
+#Type_of_Loan - Needs additioanl data cleaning
+Type_of_Loan_Anova <- aov(Type_of_Loan ~ Credit_Score, data = data_raw)
+
+#Delay_from_due_date
+Delay_from_due_date_Anova <- aov(Delay_from_due_date ~ Credit_Score, data = data_raw)
+Delay_from_due_date_Anova_Table <- summary(Delay_from_due_date_Anova)
+Delay_from_due_date_Anova_Table_var1 <- Delay_from_due_date_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Delay_from_due_date_Anova_var2 <- sum(Delay_from_due_date_Anova_Table[[1]][, "Sum Sq"])
+Delay_from_due_date_r_squared <- Delay_from_due_date_Anova_Table_var1 / Delay_from_due_date_Anova_var2
+
+#Num_of_Delayed_Payment - Needs additional data cleaning
+Num_of_Delayed_Payment_Anova <- aov(Num_of_Delayed_Payment ~ Credit_Score, data = data_raw)
+
+#Changed_Credit_Limit - Needs additional data cleaning
+Changed_Credit_Limit_Anova <- aov(Changed_Credit_Limit ~ Credit_Score, data = data_raw)
+
+#Credit_Mix - Needs additional data cleaning
+Credit_Mix_Anova <- aov(Credit_Mix ~ Credit_Score, data = data_raw)
+
+#Outstanding_Debt - Needs additional data cleaning
+Outstanding_Debt_Anova <- aov(Outstanding_Debt ~ Credit_Score, data = data_raw)
+
+#Credit_Utilization_Ratio
+Credit_Utilization_Ratio_Anova <- aov(Credit_Utilization_Ratio ~ Credit_Score, data = data_raw)
+Credit_Utilization_Ratio_Anova_Table <- summary(Credit_Utilization_Ratio_Anova)
+Credit_Utilization_Ratio_Anova_Table_var1 <- Credit_Utilization_Ratio_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Credit_Utilization_Ratio_Anova_var2 <- sum(Credit_Utilization_Ratio_Anova_Table[[1]][, "Sum Sq"])
+Credit_Utilization_Ratio_r_squared <- Credit_Utilization_Ratio_Anova_Table_var1 / Credit_Utilization_Ratio_Anova_var2
+
+#Credit_History_Age - Needs additional data cleaning
+Credit_History_Age_Anova <- aov(Credit_History_Age ~ Credit_Score, data = data_raw)
+
+#Payment_of_Min_Amount - Needs additional data cleaning
+Payment_of_Min_Amount_Anova <- aov(Payment_of_Min_Amount ~ Credit_Score, data = data_raw)
+
+#Total_EMI_per_month
+Total_EMI_per_month_Anova <- aov(Total_EMI_per_month ~ Credit_Score, data = data_raw)
+Total_EMI_per_month_Anova_Table <- summary(Total_EMI_per_month_Anova)
+Total_EMI_per_month_Anova_Table_var1 <- Total_EMI_per_month_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Total_EMI_per_month_Anova_var2 <- sum(Total_EMI_per_month_Anova_Table[[1]][, "Sum Sq"])
+Total_EMI_per_month_r_squared <- Total_EMI_per_month_Anova_Table_var1 / Total_EMI_per_month_Anova_var2
+
+#Amount_invested_monthly - Needs additional data cleaning
+Amount_invested_monthly_Anova <- aov(Amount_invested_monthly ~ Credit_Score, data = data_raw)
+
+#Payment_Behaviour - Needs additional data cleaning
+Payment_Behaviour_Anova <- aov(Payment_Behaviour ~ Credit_Score, data = data_raw)
+
+#Monthly_Balance
+Monthly_Balance_Anova <- aov(Monthly_Balance ~ Credit_Score, data = data_raw)
+Monthly_Balance_Anova_Table <- summary(Monthly_Balance_Anova)
+Monthly_Balance_Anova_Table_var1 <- Monthly_Balance_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Monthly_Balance_Anova_var2 <- sum(Monthly_Balance_Anova_Table[[1]][, "Sum Sq"])
+Monthly_Balance_r_squared <- Monthly_Balance_Anova_Table_var1 / Monthly_Balance_Anova_var2
+
+#Occupation - Not Available
+Occupation_Anova <- aov(Occupation ~ Credit_Score, data = data_raw)
+
+#Monthly_Inhand_Salary
+Monthly_Inhand_Salary_Anova <- aov(Monthly_Inhand_Salary ~ Credit_Score, data = data_raw)
+Monthly_Inhand_Salary_Anova_Table <- summary(Monthly_Inhand_Salary_Anova)
+Monthly_Inhand_Salary_Anova_Table_var1 <- Monthly_Inhand_Salary_Anova_Table[[1]]["Credit_Score", "Sum Sq"]
+Monthly_Inhand_Salary_Anova_var2 <- sum(Monthly_Inhand_Salary_Anova_Table[[1]][, "Sum Sq"])
+Monthly_Inhand_Salary_r_squared <- Monthly_Inhand_Salary_Anova_Table_var1 / Monthly_Inhand_Salary_Anova_var2
+#CATAGORICAL
+#Payment_Behavior
+pay_b_fill_colors <- c("Good" = "skyblue", "Standard" = "lightgreen", "Poor" = "salmon")
+payment_behaviour_vs_credit_score <- data_raw %>%
+  select(Payment_Behaviour, Credit_Score) %>%
+  count(Credit_Score, Payment_Behaviour) %>%
+  ggplot(aes(x = Payment_Behaviour, y = n, fill = Credit_Score)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Payment Behavior", y = "Count", title = "Count of Instances by Credit Score and Payment Behavior") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_manual(values = pay_b_fill_colors) +
+  guides(fill = guide_legend(title = "Credit Score"))
+
+#Credit Mix
+credit_mix_fill_colors <- c("Bad" = "orange", "Good" = "darkblue", "Standard" = "green", "_" = "grey")
+credit_mix_vs_credit_score <- data_raw %>%
+  select(Credit_Mix, Credit_Score) %>%
+  count(Credit_Score, Credit_Mix) %>%
+  ggplot(aes(x = Credit_Score, y = n, fill = Credit_Mix)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Credit Score", y = "Count", title = "Count of Instances by Credit Score and Credit Mix") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_manual(values = credit_mix_fill_colors) +
+  guides(fill = guide_legend(title = "Credit Mix"))
+#Num_of_Bank_Accounts
+Num_of_Bank_Accounts_vs_Credit_Score <- data_raw %>%
+  mutate(Credit_Score_Numeric = as.numeric(factor(Credit_Score, levels = c("Poor", "Standard", "Good"), ordered = TRUE))) %>%
+  ggplot(aes(x = Num_Bank_Accounts, y = Credit_Score_Numeric)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +  # Add linear trend line
+  scale_y_continuous(breaks = 1:3, labels = c("Poor", "Standard", "Good")) +  # Custom y-axis labels
+  labs(x = "Num Bank Accounts", y = "Credit Score", title = "Scatter Plot of Credit Score vs. Num Bank Accounts")
+
+#NUMERIC
+#Num_Credit_Inquires (with trend line)
+num_credit_inquires_vs_credit_score <- data_raw %>%
+  mutate(Credit_Score_Numeric = as.numeric(factor(Credit_Score, levels = c("Poor", "Standard", "Good"), ordered = TRUE))) %>%
+  ggplot(aes(x = Num_Credit_Inquiries, y = Credit_Score_Numeric)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +  # Add linear trend line
+  scale_y_continuous(breaks = 1:3, labels = c("Poor", "Standard", "Good")) +  # Custom y-axis labels
+  labs(x = "Num_Credit_Inquiries", y = "Credit_Score", title = "Scatter Plot of Credit Score vs. Num Credit Inquiries")
 
 
 
